f486cd5c0f801f14fa35b3a1dba55ca3d1a0a87f
ElviaEchauri <echaur.ely@gmail.com>
2024-02-14 12:37:06 -0600

    use pandas lib in app proyect

--- app/main.py
+++ app/main.py
@@ -1,19 +1,31 @@
 import utils 
 import read_csv
 import charts
-
+import pandas as pd
 
 def run():
-  data = read_csv.readCsv('data.csv')
+  
+  
 
+  '''
   #filtro para hacer legible el grfico de pastel
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
   #grafica de pastel
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+  #dataframe
+  df = pd.read_csv('data.csv') #funcion en Pandas para leer csv
+  df = df[df['Continent'] == 'Africa'] #forma de filtrar datos por contenido en una columna
+
+  #extraccion de datos
+  countries = df['Country/Territory'].values #funcion en pandas para optener valores de un diccionario
+  percentages = df['World Population Percentage'].values
+
   charts.generatePieChart(countries, percentages)
 
+  data = read_csv.readCsv('data.csv')
   #grafica de barras
   country = input('Type Country => ')
 
