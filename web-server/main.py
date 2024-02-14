cd2782f88a9b55c9fcb266eea35c111540c93d80
ElviaEchauri <echaur.ely@gmail.com>
2024-02-14 15:35:38 -0600

    we-server launched

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI #importar la app
+from fastapi.responses import HTMLResponse
+
+#instanciamiento de la app
+app = FastAPI()
+
+#Recurso
+@app.get('/') #adornador
+def getList():
+    return [1,2,3]
+
+@app.get('/contact',response_class = HTMLResponse)
+def getList():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>Soy un parrafo</p>
+    """
 
 def run():
     store.getCategories()
